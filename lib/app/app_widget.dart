2bd2b91c81e4adb3ec6446b5ecadb2f10a6b3ec5
regisksc <41172158+regisksc@users.noreply.github.com>
2021-04-14 02:04:36 -0300

    transformed from stateful to stateless

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,29 +1,18 @@
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, this.title}) : super(key: key);
-
-  final String? title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key, this.title = 'Getx Youtube'}) : super(key: key);
   int _counter = 0;
+  final String title;
 
   void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+    _counter++;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title!),
-      ),
+      appBar: AppBar(title: Text(title)),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
